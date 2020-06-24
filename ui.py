2cb80b05722643704c61f3b3ca1013854cabdfc9
Naoimi Gillis <naoimigillis@gmail.com>
2020-06-24 19:11:24 +0100

    Added to UI to get working for bmi_prop

--- ui.py
+++ ui.py
@@ -1,39 +1,42 @@
 import tkinter as tk
 from tkinter import ttk
 import locale
-from bmi import Bmi
+
+from bmi_prop import BmiProperty
 
 class BmiInterfaces(ttk.Frame):
-    def __init__(self,parent):
-        ttk.Frame.__init__(self,parent)
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent)
 
-        BmiInterface(parent).grid(row=0,column=0)
-        BmiInterface(parent).grid(row=0,column=0)
+        BmiInterface(parent).grid(row=0, column=0)
+        BmiInterface(parent).grid(row=0, column=1)
 
-        #Add the Calculate button
-        ttk.Button(parent,text="Calculate",command=parent.destroy).grid(row=1,column=1,sticky=tk.E,padx=15,pady=10)
+        # Add the Exit button
+        #ttk.Button(parent, text="Exit", command=parent.destroy).grid(row=1, column=1, sticky=tk.E, padx=15, pady=10)
 
 class BmiInterface(ttk.Frame):
-    def __init__(self,parent):
-        ttk.Frame.__init__(self,parent,padding="10 10 10 10")
+    def __init__(self, parent):
+        ttk.Frame.__init__(self, parent, padding="10 10 10 10")
         self.parent = parent
-        self.bmi= BMI()
-        result=locale.setlocale(locale.LC_ALL,'en_US')
+        self.bmi_prop = BmiProperty()
+        result = locale.setlocale(locale.LC_ALL, '')
+        if result == 'C':
+            locale.setlocale(locale.LC_ALL, 'en_US')
 
-        #Define string variables for text entry fields
-        self.stone=tk.StringVar()
-        self.pounds=tk.StringVar()
-        self.feet=tk.StringVar()
-        self.inches=tk.StringVar()
-        self.kgs=tk.StringVar()
-        self.cms=tk.StringVar()
+        # Define string variables for text entry fields
+        self.stone = tk.StringVar()
+        self.pounds = tk.StringVar()
+        self.feet = tk.StringVar()
+        self.inches = tk.StringVar()
+        self.kgs = tk.StringVar()
+        self.cms = tk.StringVar()
 
         self.initComponents()
 
     # Display the grid of labels and text entry fields
     def initComponents(self):
-        ttk.Label(self,text="Stone").grid(column=0,row=0,sticky=tk.E)
-        ttk.Entry(self,width=25,textVariable=self.stone).grid(column=1,row=0)
+        ttk.Label(self, text="Stone").grid(column=0, row=0, sticky=tk.E)
+        ttk.Entry(self, width=25, textVariable=self.stone).grid(column=1, row=0)
 
         ttk.Label(self, text="Feet").grid(column=0, row=1, sticky=tk.E)
         ttk.Entry(self, width=25, textVariable=self.feet).grid(column=1, row=2)
@@ -53,22 +56,24 @@ class BmiInterface(ttk.Frame):
         self.makeButtons()
 
         for child in self.winfo_children():
-            child._grid_configure(padx=5,pady=3)
+            child._grid_configure(padx=5, pady=3)
 
     def makeButtons(self):
-        buttonFrame=ttk.Frame(self)
-        buttonFrame.grid(column=4,row=6,columnspan=2,sticky=tk.E)
-        ttk.Button(buttonFrame,text="Clear", command=self.clear).grid(column=0,row=0,padx=5)
-        ttk.Button(buttonFrame, text="Caluclate your BMI", command=self.calculate).grid(column=1, row=0, padx=5)
+        buttonFrame = ttk.Frame(self)
+        buttonFrame.grid(column=4, row=6, columnspan=2, sticky=tk.E)
 
+        ttk.Button(buttonFrame, text="Clear", command=self.clear).grid(column=0, row=0, padx=5)
+        ttk.Button(buttonFrame, text="Calculate your BMI", command=self.calculate).grid(column=1, row=0, padx=5)
 
     def calculate(self):
-        self.bmi._stone=float(self.stone.get())
-        self.bmi._pounds = float(self.pounds.get())
-        self.bmi._feet=float(self.feet.get())
-        self.bmi._inches = float(self.inches.get())
-        self.bmi._kgs = float(self.kgs.get())
-        self.bmi._cms = float(self.cms.get())
+        #self.bmi_prop._stone = float(self.stone.get())
+        #self.bmi_prop._pounds = float(self.pounds.get())
+        #self.bmi_prop._feet = float(self.feet.get())
+        #self.bmi_prop._inches = float(self.inches.get())
+
+
+        self.bmi_prop._kgs = float(self.kgs.get())
+        self.bmi_prop._cms = float(self.cms.get())
 
     def clear(self):
         self.stone.set("")
@@ -78,8 +83,9 @@ class BmiInterface(ttk.Frame):
         self.kgs.set("")
         self.cms.set("")
 
+
 if __name__ == " main ":
-    root = tk.Tk
+    root = tk.Tk()
     root.title("BMI Calculator")
     BmiInterfaces(root)
     root.mainloop()
